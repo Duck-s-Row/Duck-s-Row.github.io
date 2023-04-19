df787d0edcf432be4f199ef42a215ee49a4cd29e
Abdalla28 <109187325+Abdalla28@users.noreply.github.com>
2023-04-20 01:38:13 +0200

    edit profile page & my plan

--- Profile/app.js
+++ Profile/app.js
@@ -1,12 +1,10 @@
-let date = document.getElementById("date");
+
 let chooice = document.querySelectorAll(".chooice");
 let details_info = document.getElementById("details_info");
-let edit_info = document.getElementById("edit_info");
-let favorites = document.getElementById("favorites");
+let pass = document.getElementById("pass");
 let planes = document.getElementById("planes");
 let det = document.getElementById("det");
 
-date.defaultValue = "2023-02-23";
 
 chooice.forEach(button => {
     button.addEventListener("click", () => {
@@ -24,28 +22,18 @@ function resetlinks() {
 chooice[0].addEventListener("click", () => {
     details_info.style.display = 'block';
     planes.style.display = 'none';
-    edit_info.style.display = 'none';
-    favorites.style.display = 'none';
+    pass.style.display = 'none';
 });
 
 chooice[1].addEventListener("click", () => {
     details_info.style.display = 'none';
     planes.style.display = 'block';
-    edit_info.style.display = 'none';
-    favorites.style.display = 'none';
+    pass.style.display = 'none';
 });
 
 
 chooice[2].addEventListener("click", () => {
     details_info.style.display = 'none';
     planes.style.display = 'none';
-    edit_info.style.display = 'block';
-    favorites.style.display = 'none';
-});
-
-chooice[3].addEventListener("click", () => {
-    details_info.style.display = 'none';
-    planes.style.display = 'none';
-    edit_info.style.display = 'none';
-    favorites.style.display = 'block';
+    pass.style.display = 'block';
 });
