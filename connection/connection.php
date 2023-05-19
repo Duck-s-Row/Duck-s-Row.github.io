39624f65befca2bbc114f4a7e31ad2c146b11fa5
3bdalrahman <abdomoslaam@gmail.com>
2023-05-19 20:04:17 +0300

    Update connection.php

--- connection/connection.php
+++ connection/connection.php
@@ -2,7 +2,7 @@
 
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpassword = "boudy126667774479";
+$dbpassword = "Abdo1234";
 $dbname = "ducks_row";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname))
