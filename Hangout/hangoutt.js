3546f19cb3e17c358d7a0d748602e7dabc674edc
3bdalrahman <abdomoslaam@gmail.com>
2023-05-20 11:02:43 +0300

    Update hangoutt.js

--- Hangout/hangoutt.js
+++ Hangout/hangoutt.js
@@ -1,13 +1,3 @@
-// let button = document.getElementById("more");
-// let popup = document.querySelector(".popup");
-// let exit = document.getElementById("exit");
-
-// button.addEventListener("click", () =>{
-//   popup.style.display = "flex";
-// })
-// exit.addEventListener("click", () =>{
-//   popup.style.display = "none";
-// })
 let range = document.getElementById("rangeVlaue");
 function rangeChange(value) {
   range.innerHTML = value;
