350687a4b97841f0782a133fb2e50cd197d34d21
Abdalla28 <109187325+Abdalla28@users.noreply.github.com>
2023-05-27 13:13:10 +0300

    jnjkn

---
+++ request/request.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="request.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- The Start of Navbar section -->
+    <header>
+        <div class="logo"><a href="../index.php"><img src="../home/imgs/ducks.png" alt=""></a></div>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <nav class="nav-bar">
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../plans/plans.php">My Plans</a></li>
+                <!-- <li><a href="Sign_UP/first page/Sign_up.php">My Planes</a></li> -->
+                <li><a href="#contact_us">About</a></li>
+                <li><a href="../Profile/profile.php" class="profile">Profile</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!-- The End of Navbar section -->
+    <section class="main">
+
+    </section>
+</body>
+</html>
