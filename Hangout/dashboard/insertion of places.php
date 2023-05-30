d4b414948e9955e564c58c7d94bc24b6b2bed9ed
3bdalrahman <abdomoslaam@gmail.com>
2023-05-30 22:49:59 +0300

    Update insertion of places.php

--- Hangout/dashboard/insertion of places.php
+++ Hangout/dashboard/insertion of places.php
@@ -113,6 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="website icon" type="png" href="../../home/imgs/Logo.png">
     <title>insertion</title>
 </head>
 
