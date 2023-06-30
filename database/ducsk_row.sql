e9d88d568e4b73a954c596c67d3bbc4bbc9083f1
3bdalrahman <abdomoslaam@gmail.com>
2023-06-30 03:23:13 +0300

    Update ducsk_row.sql

--- database/ducsk_row.sql
+++ database/ducsk_row.sql
@@ -170,6 +170,7 @@ comment varchar(255)
 );
 alter table request_comment add foreign key(place_id) references request_details(place_id);
 
+-- if does not work drop it manullay 
 ALTER TABLE `ducks_row`.`exist_plan` DROP FOREIGN KEY `exist_plan_ibfk_2`;
 ALTER TABLE `ducks_row`.`exist_plan` DROP COLUMN `user_id`,
     DROP INDEX `user_id`;
