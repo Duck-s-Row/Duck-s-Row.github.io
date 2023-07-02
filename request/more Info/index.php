ea78d32cfaa6065bac6ae4e1289dd5f1bc0a962e
Abdalla28 <109187325+Abdalla28@users.noreply.github.com>
2023-07-02 19:55:25 +0300

    مش فاكر زي عبضو

--- request/more Info/index.php
+++ request/more Info/index.php
@@ -63,111 +63,123 @@ $place_id = $eachDetail['place_id'];
         </nav>
     </header>
     <section class="request_details">
-        <?php
-        $selectPlaceIdQuery = "SELECT place_id From request_details WHERE request_id = $request_id";
-        $resultPlace_id = mysqli_query($con, $selectPlaceIdQuery);
-        $rowPlace_id = mysqli_fetch_assoc($resultPlace_id);
-        $place_id = $rowPlace_id['place_id'];
-        //picture query
-        $pics_query = "SELECT * FROM request_place_pics WHERE place_id = $place_id ORDER BY RAND() ";
-        $result_pics = mysqli_query($con, $pics_query);
-        ?>
-
-        <div class="photo">
-            <img src="../../Hangout/logos/<?php echo $eachDetail['logo'] ?>" alt="">
-            <div class="slider-container">
-                <div id="slide-number" class="slide-number"></div>
-                <?php
-                if ($result_pics->num_rows > 0) {
-                    while ($row_pics = $result_pics->fetch_assoc()) :
-                        // $imageURL = '../places_imgs/'.$row_pics['photo_name']; 
-                ?>
-                        <img src="../../Hangout/places_imgs/<?php echo $row_pics['photo_name'] ?>" alt="">
-                <?php endwhile;
-                }  ?>
-            </div>
-
-            <div class="slider-controls">
-                <span id="prev" class="prev"><i class="fa-solid fa-circle-arrow-left"></i></span>
-                <span id="indicators" class="indicators"></span>
-                <span id="next" class="next"><i class="fa-solid fa-circle-arrow-right"></i></span>
-            </div>
-        </div>
-        <script src="slider.js"></script>
-        <form action="adjust request.php" method="post">
+        <div class="left">
             <?php
-            $selectComQuery = "SELECT * FROM request_comment WHERE place_id = $place_id";
-            $AllCom = mysqli_query($con, $selectComQuery);
-            if (mysqli_num_rows($AllCom) > 0) :
+            $selectPlaceIdQuery = "SELECT place_id From request_details WHERE request_id = $request_id";
+            $resultPlace_id = mysqli_query($con, $selectPlaceIdQuery);
+            $rowPlace_id = mysqli_fetch_assoc($resultPlace_id);
+            $place_id = $rowPlace_id['place_id'];
+            //picture query
+            $pics_query = "SELECT * FROM request_place_pics WHERE place_id = $place_id ORDER BY RAND() ";
+            $result_pics = mysqli_query($con, $pics_query);
             ?>
-                <div class="com-table">
-                    <table>
-                        <tr>
-                            <th>Comment</th>
-                            <th>Comment Date</th>
-                        </tr>
-                        <?php while ($eachCom = mysqli_fetch_assoc($AllCom)) : ?>
+            <div class="log">
+                <img src="../../Hangout/logos/<?php echo $eachDetail['logo'] ?>" alt="" width="70" height="70">
+            </div>
+            
+            <div class="main-slide">
+                <div class="slider-container">
+                    <div id="slide-number" class="slide-number"></div>
+                    <?php
+                    if ($result_pics->num_rows > 0) {
+                        while ($row_pics = $result_pics->fetch_assoc()) :
+                            // $imageURL = '../places_imgs/'.$row_pics['photo_name']; 
+                    ?>
+                            <img src="../../Hangout/places_imgs/<?php echo $row_pics['photo_name'] ?>" alt="">
+                    <?php endwhile;
+                    }  ?>
+                </div>
+
+                <div class="slider-controls">
+                    <span id="prev" class="prev"><i class="fa-solid fa-angles-left"></i></span>
+                    <span id="indicators" class="indicators"></span>
+                    <span id="next" class="next"><i class="fa-solid fa-angles-right"></i></span>
+                </div>
+            </div>
+            <script src="slider.js"></script>
+            <?php
+                $selectComQuery = "SELECT * FROM request_comment WHERE place_id = $place_id";
+                $AllCom = mysqli_query($con, $selectComQuery);
+                if (mysqli_num_rows($AllCom) > 0) :
+                ?>
+                    <div class="com-table">
+                        <table>
                             <tr>
-                                <td><?php echo $eachCom['comment'] ?></td>
-                                <td><?php echo $eachCom['com_date'] ?></td>
+                                <th>Comment</th>
+                                <th>Comment Date</th>
                             </tr>
-                        <?php endwhile; ?>
-                    </table>
+                            <?php while ($eachCom = mysqli_fetch_assoc($AllCom)) : ?>
+                                <tr>
+                                    <td><?php echo $eachCom['comment'] ?></td>
+                                    <td><?php echo $eachCom['com_date'] ?></td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </table>
+                    </div>
+                <?php endif; ?>
+        </div>
+        <div class="right">
+            <form action="adjust request.php" method="post">
+                
+                <div>
+                    <input type="hidden" name="place_id" value="<?php echo $eachDetail['place_id'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
+                    <label for="p_name">Place Name</label>
+                    <input type="text" name="p_name" id="p_name" value="<?php echo $eachDetail['p_name'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
                 </div>
-            <?php endif; ?>
-            <div>
-                <input type="hidden" name="place_id" value="<?php echo $eachDetail['place_id'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
-                <label for="p_name">Place Name</label>
-                <input type="text" name="p_name" id="p_name" value="<?php echo $eachDetail['p_name'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
-            </div>
-            <div>
-                <label for="category">Category</label>
-                <select name="category" id="category" <?php if ($eachDetail['req_status'] == "Accepted") echo "disabled"; ?>>
-                    <option value="Cafe" <?php if ($eachDetail['category'] == "Cafe") echo "selected"; ?>>Cafe</option>
-                    <option value="Restaurants" <?php if ($eachDetail['category'] == "Restaurants") echo "selected"; ?>>Restaurants</option>
-                    <option value="Park" <?php if ($eachDetail['category'] == "Park") echo "selected"; ?>>Park</option>
-                    <option value="Museums" <?php if ($eachDetail['category'] == "Museums") echo "selected"; ?>>Museums</option>
-                    <option value="Mall" <?php if ($eachDetail['category'] == "Mall") echo "selected"; ?>>Mall</option>
-                    <option value="Cinema" <?php if ($eachDetail['category'] == "Cinema") echo "selected"; ?>>Cinema</option>
-                    <option value="EscapeRoom" <?php if ($eachDetail['category'] == "Escape Rooms") echo "selected"; ?>>Escape Rooms</option>
-                </select>
-            </div>
-            <div>
-                <label for="">Budget</label>
-                <input type="number" name="min" id="min" value="<?php echo $eachDetail['min_price'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>-
-                <input type="number" name="max" id="max" value="<?php echo $eachDetail['max_price'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
-                <div id="average">
-                    <label>Average</label>
-                    <p><span id="avr"></span>L.E.</p>
+                <div>
+                    <label for="category">Category</label>
+                    <select name="category" id="category" <?php if ($eachDetail['req_status'] == "Accepted") echo "disabled"; ?>>
+                        <option value="Cafe" <?php if ($eachDetail['category'] == "Cafe") echo "selected"; ?>>Cafe</option>
+                        <option value="Restaurants" <?php if ($eachDetail['category'] == "Restaurants") echo "selected"; ?>>Restaurants</option>
+                        <option value="Park" <?php if ($eachDetail['category'] == "Park") echo "selected"; ?>>Park</option>
+                        <option value="Museums" <?php if ($eachDetail['category'] == "Museums") echo "selected"; ?>>Museums</option>
+                        <option value="Mall" <?php if ($eachDetail['category'] == "Mall") echo "selected"; ?>>Mall</option>
+                        <option value="Cinema" <?php if ($eachDetail['category'] == "Cinema") echo "selected"; ?>>Cinema</option>
+                        <option value="EscapeRoom" <?php if ($eachDetail['category'] == "Escape Rooms") echo "selected"; ?>>Escape Rooms</option>
+                    </select>
                 </div>
-            </div>
-            <div>
-                <label for="details">Details</label>
-                <input type="text" name="details" id="details" value="<?php echo $eachDetail['details'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
-            </div>
-            <div>
-                <label for="p_branch">Place Branch</label>
-                <select id="p_branch" name="p_branch" <?php if ($eachDetail['req_status'] == "Accepted") echo "disabled"; ?>>
-                    <optgroup label="GIZA">
-                        <option value="haram" <?php if ($eachDetail['p_branch'] == "haram") echo "selected"; ?>>haram</option>
-                        <option value="fisal" <?php if ($eachDetail['p_branch'] == "fisal") echo "selected"; ?>>fisal</option>
-                        <option value="el doki" <?php if ($eachDetail['p_branch'] == "el doki") echo "selected"; ?>>el doki</option>
-                        <option value="zamalek" <?php if ($eachDetail['p_branch'] == "zamalek") echo "selected"; ?>>zamalek</option>
-                        <option value="6th october" <?php if ($eachDetail['p_branch'] == "6th october") echo "selected"; ?>>6th october</option>
-                        <option value="el shiekh zayed" <?php if ($eachDetail['p_branch'] == "el shiekh zayed") echo "selected"; ?>>el shiekh zayed</option>
-                        <option value="el mohandseen" <?php if ($eachDetail['p_branch'] == "el Mohandseen") echo "selected"; ?>>el Mohandseen</option>
-                        <option value="el manial" <?php if ($eachDetail['p_branch'] == "el Manial") echo "selected"; ?>>el Manial</option>
-                    </optgroup>
-                </select>
-            </div>
-            <div>
-                <label for="location">i Frame src Location</label>
-                <input type="url" name="location" id="location" value="<?php echo $eachDetail['location'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
-            </div>
-            <div>
-                <input type="submit" value="Update" <?php if ($eachDetail['req_status'] == "Accepted") echo "disabled"; ?>>
-            </div>
-        </form>
+                <div>
+                    <label for="">Budget</label>
+                    <input type="number" name="min" id="min" value="<?php echo $eachDetail['min_price'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>-
+                    <input type="number" name="max" id="max" value="<?php echo $eachDetail['max_price'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
+                    <div id="average">
+                        <label>Average</label>
+                        <p><span id="avr"></span>L.E.</p>
+                    </div>
+                </div>
+                <div>
+                    <label for="details">Details</label>
+                    <input type="text" name="details" id="details" value="<?php echo $eachDetail['details'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
+                </div>
+                <div class="menu">
+                    <label>MENU</label>
+                    <p>Show Menu</p>
+                </div>
+                <div>
+                    <label for="p_branch">Place Branch</label>
+                    <select id="p_branch" name="p_branch" <?php if ($eachDetail['req_status'] == "Accepted") echo "disabled"; ?>>
+                        <optgroup label="GIZA">
+                            <option value="haram" <?php if ($eachDetail['p_branch'] == "haram") echo "selected"; ?>>haram</option>
+                            <option value="fisal" <?php if ($eachDetail['p_branch'] == "fisal") echo "selected"; ?>>fisal</option>
+                            <option value="el doki" <?php if ($eachDetail['p_branch'] == "el doki") echo "selected"; ?>>el doki</option>
+                            <option value="zamalek" <?php if ($eachDetail['p_branch'] == "zamalek") echo "selected"; ?>>zamalek</option>
+                            <option value="6th october" <?php if ($eachDetail['p_branch'] == "6th october") echo "selected"; ?>>6th october</option>
+                            <option value="el shiekh zayed" <?php if ($eachDetail['p_branch'] == "el shiekh zayed") echo "selected"; ?>>el shiekh zayed</option>
+                            <option value="el mohandseen" <?php if ($eachDetail['p_branch'] == "el Mohandseen") echo "selected"; ?>>el Mohandseen</option>
+                            <option value="el manial" <?php if ($eachDetail['p_branch'] == "el Manial") echo "selected"; ?>>el Manial</option>
+                        </optgroup>
+                    </select>
+                </div>
+                <div>
+                    <label for="location">i Frame src Location</label>
+                    <input type="url" name="location" id="location" value="<?php echo $eachDetail['location'] ?>" <?php if ($eachDetail['req_status'] == "Accepted") echo "readonly"; ?>>
+                </div>
+                <div>
+                    <input type="submit" value="Update" <?php if ($eachDetail['req_status'] == "Accepted") echo "disabled"; ?>>
+                </div>
+            </form>
+        </div>
+        
     </section>
     <script>
         const minInput = document.getElementById('min');
